2db8e2f1b8593493b4defcd8af5def40596e61db
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2024-04-17 11:46:54 +0100

    some changes in AvatarDisplay component and finished statusDisplay

--- monday-clone/src/components/AvatarDisplay.js
+++ monday-clone/src/components/AvatarDisplay.js
@@ -3,8 +3,9 @@ import blankAvatar from "../images/altPicture.png"
 const AvatarDisplay = ({ticket})=>{
     return(
         <div className="avatar-container">
-            <div className="imgg-container">
-              <img src={ticket.avatar ? ticket.avatar : blankAvatar} alt={'photo of' + ticket.owner} />
+            <div className="img-container">
+              {/* <img src={ticket.avatar ? ticket.avatar : blankAvatar} alt={'photo of' + ticket.owner} /> */}
+              <img src={ticket.avatar || blankAvatar} alt={'photo of' + ticket.owner} />
             </div> 
             
         </div>
