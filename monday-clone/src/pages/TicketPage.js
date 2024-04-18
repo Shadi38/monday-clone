adba1ad8fd9212c121ac27458d70cceb36d5f784
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2024-04-18 16:25:45 +0100

    start to make changes TicketPages component

--- monday-clone/src/pages/TicketPage.js
+++ monday-clone/src/pages/TicketPage.js
@@ -1,7 +1,59 @@
+import { useState} from 'react'
+
 const TicketPage = ()=>{
+    const [formData,setFormData]=useState({
+        status: 'not started',
+        progress: 0,
+        timeStamp: new Date().toISOString()
+    })
+   
+    const editMode = false;
+
+    const handleSubmit = () =>{
+        console.log('submited')
+    }
+    
+    const handleChange =(e) => {
+       const value = e.target.value
+       const name = e.target.name
+
+       setFormData((prevState)=>({
+            ...prevState,
+            [name]: value 
+       }))
+       
+    }
+
     return(
         <div className="ticket">
-            <h1>{}</h1>
+            <h1>{editMode ? 'Update your ticket' : 'Create a ticket'}</h1>
+            <div className="ticket-container">
+                <form onSubmit={handleSubmit}> 
+                   <section>
+                      <label htmlFor="title">Title</label>
+                      <input
+                         id="title"
+                         name="title"
+                         type="text"
+                         onChange={handleChange}
+                         required='true'
+                         value={formData.title}>
+
+                      </input>
+
+                      <label htmlFor="description">Description</label>
+                      <input
+                         id="description"
+                         name="description"
+                         type="text"
+                         onChange={handleChange}
+                         required='true'
+                         value={formData.description}></input>
+
+                   </section>
+                </form>
+
+            </div>
         </div>
     )
     }
